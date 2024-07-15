3335ad53d772b023160188de7969eea452b4c212
Aditya Gupta <aditya3492gupta@gmail.com>
2024-07-15 13:42:32 +0530

    day 1

--- 75.sort-colors.cpp
+++ 75.sort-colors.cpp
@@ -5,11 +5,30 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
 // @lc code=end
-
