c9318b5a7e521c16b5cbb7f0e452b472fd6fb5b1
Aditya Gupta <aditya3492gupta@gmail.com>
2024-07-26 21:51:35 +0530

    day 12

---
+++ HEAP/215.kth-largest-element-in-an-array.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=215 lang=cpp
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int findKthLargest(std::vector<int> &nums, int k)
+    {
+        priority_queue<int, vector<int>, greater<int>> min_heap(nums.begin(), nums.begin() + k);
+        for (int i = k; i < nums.size(); i++)
+        {
+            if (nums[i] > min_heap.top())
+            {
+                min_heap.pop();
+                min_heap.push(nums[i]);
+            }
+        }
+        return min_heap.top();
+    }
+};
+// @lc code=end
---
+++ SORTING AND SEARCHING/162.find-peak-element.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=162 lang=cpp
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+};
+// @lc code=end
