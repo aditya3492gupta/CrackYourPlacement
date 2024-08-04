87cd71a1b596658d02d126a294177ea41e60a2f1
Aditya Gupta <aditya3492gupta@gmail.com>
2024-08-04 23:16:32 +0530

    day 21

---
+++ BACKTRACKING/47.permutations-ii.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=47 lang=cpp
+ *
+ * [47] Permutations II
+ */
+
+// @lc code=start
+class Solution
+{
+private:
+    void permuteUnique(vector<int> &nums, vector<vector<int>> &output, vector<int> temp, vector<bool> &visited)
+    {
+        if (temp.size() == nums.size())
+        {
+            output.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (visited[i] || i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
+                continue;
+            visited[i] = true;
+            temp.push_back(nums[i]);
+            permuteUnique(nums, output, temp, visited);
+            temp.pop_back();
+            visited[i] = false;
+        }
+    }
+
+public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        vector<int> temp;
+        vector<bool> visited(nums.size(), 0);
+        permuteUnique(nums, output, temp, visited);
+        return output;
+    }
+};
+// @lc code=end
